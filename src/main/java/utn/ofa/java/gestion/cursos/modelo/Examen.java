255259554881cfed28a481fd4bfee86695d1e7e3
mdomingu <mdomingu@gmailcom>
2018-06-03 22:10:53 -0300

    commit inicial

---
+++ src/main/java/utn/ofa/java/gestion/cursos/modelo/Examen.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utn.ofa.java.gestion.cursos.modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author marti
+ */
+public class Examen {
+    private Integer id;
+    private Date fecha;
+    private Integer nota;
+    private Curso  curso;
+    private Docente docente;
+    private Alumno alumno;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+    
+    
+}
